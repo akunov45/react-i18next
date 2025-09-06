c1be3a4db3e61fa37ab79d1c70d37f176a5ecdb3
Akunov4594 <86850933+akunov45@users.noreply.github.com>
2025-09-06 11:11:48 +0600

    up

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,25 @@ import "./i18n"
 import { useTranslation } from "react-i18next"
 
 const App = () => {
-  const {t} = useTranslation()
+  const { t, i18n } = useTranslation()
+
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang.toLowerCase()); // EN -> en
+  }
 
   return (
     <div>
+      <div style={{ display: "flex", gap: 10 }}>
+        <button onClick={() => changeLanguage("EN")}>EN</button>
+        <button onClick={() => changeLanguage("RU")}>RU</button>
+        <button onClick={() => changeLanguage("KG")}>KG</button>
+      </div>
+      <select  onChange={(e) => changeLanguage(e.target.value)}>
+        <option value="en">EN</option>
+        <option value="ru">RU</option>
+        <option value="kg">KG</option>
+      </select>
+
       <h3>{t("Welcome")}</h3>
     </div>
   )
